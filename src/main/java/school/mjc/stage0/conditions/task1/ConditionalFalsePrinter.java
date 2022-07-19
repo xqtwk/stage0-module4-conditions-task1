65c4b34b3b4c6bdcec551a0bfed71616efb585ad
Richard <xqtwkk@gmail.com>
2022-07-20 00:37:26 +0300

    Attempt 1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (1 > 2){} else {
+            System.out.println(false);
+        }
     }
 }
